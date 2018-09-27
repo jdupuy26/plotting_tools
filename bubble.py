7ed27d52279f777ee2b7d63e35b6c60640d89afb
John Dupuy <jdupuy26@gmail.com>
2018-09-27 09:37:47 -0400

    Add shell mode when applying bubble to data
    Shell mode will take the mass in the bubble and place it on the boundary of the bubble. At the boundary, an expansion velocity is assumed, and the temperature is determined from Teq

--- bubble.py
+++ bubble.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt 
+from scipy import ndimage 
+
 # the following file assumes units: [pc, Myr, & solar masses] throughout
 
 class Bubble(object):
@@ -29,13 +31,17 @@ class Bubble(object):
         # initialize bubble position & size
         self.pos = np.array([ np.random.uniform(self.x1min,self.x1max), 
                               np.random.uniform(self.x2min,self.x2max) ])
-        sizescale = 0.25e3 # [pc]
+        sizescale = 0.175e3 # [pc]
+        #sizescale = 1.0e3
         self.size = np.random.exponential(size=1,scale=sizescale)[0] 
 
         # initialize velocity of bubble 
-        self.vel  = np.random.uniform(4.,30.) # pc/Myr  
+        self.vel  = np.random.uniform(10.,30.) # [pc/Myr]  
 
-    def apply_bubble(self, quant, data, X, Y):
+        # initialize rotation rate 
+        self.omg  = 0.0337 # [1/Myr] 
+
+    def apply_bubble(self, quant, data, X, Y, mode='default', bub_dens=None, fTeq=None):
         """
         Takes in a single snapshot of 
         simulation data and places the bubble on the
@@ -47,6 +53,12 @@ class Bubble(object):
             data: array of grid data
             X: array of cell-centered X values (size (nx2,nx1))
             Y: array of cell-centered Y values (size (nx2,nx1))
+            mode: mode for applying bubble
+            bub_dens: array of bub_dens data (to be used to get Teq)
+            fTeq: 1D interpolating function for equilibrium temp calculation.
+                  this is computed in plot_sims from the file mean_Teq.dat.
+                  It is an average over the metallicity gradient so that 
+                  Teq = Teq(d) only.   
 
         Output:
             bubble_data: data after applying bubble 
@@ -60,19 +72,75 @@ class Bubble(object):
         bubble_data = data.copy()
 
         # quantity specific things
-        if quant == 'd':
-            bubble_data[:,mask] -= data[:,mask] - 1.0 # add small background density  
+        if mode == 'default':
+            if quant == 'd':
+                bubble_data[:,mask] -= data[:,mask] - 1.0 # add small background density  
 
-        elif quant == 'v1':
-            bubble_data[:,mask] -= data[:,mask] - 1.0 # add small velocity background
+            elif quant == 'v1':
+                bubble_data[:,mask] -= data[:,mask] - 1.0 # add small velocity background
+                
+            elif quant == 'v2':
+                bubble_data[:,mask] -= data[:,mask] - 1.0 # add small velocity background  
+
+            elif quant == 'T':
+                bubble_data[:,mask] -= data[:,mask] - 1e7 # add high temperature 
+
+            else:
+                raise Exception('[UnknownQuant]: do not know how to apply {} to data'.format(quant))
+
+        # high density shell w/ expansion velocity  
+        elif mode == 'shell':
+            # step 1: convert mask to binary  
+            bindata = mask.astype(int) 
+            # step 2: compute boundary mask
+            bmsk   = ndimage.laplace(bindata) > 0 
+            # step 3: get size of boundary mask
+            bmsk_size = float(bubble_data[:,bmsk].size)
+
+            if quant == 'd':
+                bubble_data[:,mask] -= data[:,mask] - 1.0
+                # place a high density shell at the boundary  
+                if bmsk_size > 0: 
+                    bubble_data[:,bmsk]  = np.sum(data[:,mask])/bmsk_size  
+
+            elif quant == 'v1' or quant == 'v2':
+                bubble_data[:,mask] -= data[:,mask] - 1.0
+
+                # now compute the expansion velocity for the boundary
+                    # step 1: get coords of boundary in sim frame
+                xbnds = X[bmsk]
+                ybnds = Y[bmsk] 
+                rbnds = np.sqrt( xbnds**2. + ybnds**2. )
+                    # step 2: get coords of boundary in bub frame
+                xpbnds = xbnds - xpos
+                ypbnds = ybnds - ypos    
+                    # step 3: get gamma angle 
+                gam = np.arctan2(ypbnds, xpbnds)
+                    # step 4: compute vx, vy 
+                vx = self.vel*np.cos(gam)
+                vy = self.vel*np.sin(gam) 
+                    # step 5: convert to R, \phi vel
+                vr = (xbnds/rbnds)*vx + (ybnds/rbnds)*vy
+                vp = (xbnds/rbnds)*vy - (ybnds/rbnds)*vx
+                    # step 6: set velocity @ the boundary 
+                if quant == 'v1':
+                    bubble_data[:,bmsk] = vr
+                else:
+                    bubble_data[:,bmsk] = vp - self.omg*rbnds # convert to vel in rotating frame  
+
+            elif quant == 'T':
+                bubble_data[:,mask] -= data[:,mask] - 1e7
+
+                # now compute equilibrium temperature for the boundary (requires bub_dens)  
+                if bub_dens is None: 
+                    bub_dens = self.apply_bubble('d', data, X, Y, mode=mode)  
+
+                if bmsk_size > 0: 
+                    bubble_data[:,bmsk]  = fTeq(bub_dens[:,bmsk])  
             
-        elif quant == 'v2':
-            bubble_data[:,mask] -= data[:,mask] - 1.0 # add small velocity background  
-
-        elif quant == 'T':
-            bubble_data[:,mask] -= data[:,mask] - 1e7 # add high temperature 
+            else:
+                raise Exception('[UnknownQuant]: do not know how to apply {} to data'.format(quant))
 
         else:
-            raise Exception('[UnknownQuant]: do not know how to apply {} to data'.format(quant))
-
+            raise Exception('[UnknownMode]: mode {} not understood'.format(mode)) 
         return bubble_data 
